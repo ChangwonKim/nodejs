2960c542779e291fc85375f7cb9eca8bcc287bee
ChangwonKim <doserack@gmail.com>
2020-07-26 17:39:01 +0900

    pug

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookeParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./Middleware";
+import routes from "./routes"
 import userRouter from "./routers/userRouter"
 import viderRouter from "./routers/videoRouter"
 import globalRouter from "./routers/globalRouter"
-import routes from "./routes"
-const app = express();
 
+const app = express();
 //middle ware(중간 처리 함수)
+
+app.use(helmet());
+app.set("view engine","pug");
 app.use(cookeParser());
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true})); 
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware); //전역변수
 
 //router연결(각각의 웹페이지로 분할)
 app.use(routes.home, globalRouter);
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,14 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
-export const vidoes = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+//export const home = (req, res) => res.sendFile("../");
+export const home = (req, res) => res.render("home", {pageTitle:"Home"});
+
+export const search = (req, res) => {
+    //const searchingBy = req.query.term; Before ES6
+    const {query : {term : searchingBy}} = req;
+    res.render("search", {pageTitle:"Search", searchingBy});
+}
+
+export const vidoes = (req, res) => res.render("videos", {pageTitle:"Vidoes"});
+export const upload = (req, res) => res.render("upload", {pageTitle:"Upload"});
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle:"Video Detail"});
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle:"Edit Video"});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle:"Delete Video"}); 
