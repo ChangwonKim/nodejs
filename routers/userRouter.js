538f35df99d1c64b5375407741f52a5bdc552f08
ChangwonKim <doserack@gmail.com>
2020-07-30 01:45:11 +0900

    20200730

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,15 @@
 import express from "express"
 import routes from "../routes"
-import { users, changePassword, editProfile, userDetail } from "../controllers/userController";
+import { users, 
+         changePassword, 
+         editProfile, 
+         userDetail } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
-userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword)
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
+//userRouter.get(routes.users, users);
 
 export default userRouter;
